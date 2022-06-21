b722ace5e5bf626509ad6acd06db87775d560e29
Artem V.R <artra@ya.ru>
2022-06-21 07:23:10 +0300

    IDEA: gradle installdist (now app running)

--- app/src/main/java/hexlet.code/App.java
+++ app/src/main/java/hexlet.code/App.java
@@ -1,6 +1,6 @@
-//package 
+package hexlet.code;
 class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Welcome to the Brain Games!");
 	}
 }
