49478daff6d2b038afaf29d3695f32ac6619a762
Artem V.R <artra@ya.ru>
2022-08-23 07:43:54 +0300

    IDEA: corrections after 7th review

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -5,7 +5,8 @@ import hexlet.code.Utils;
 import java.util.StringJoiner;
 
 public class Progression {
-    static final int SEQUENCE_LENGTH = 10;
+    static final int PROGRESSION_LENGTH_MIN = 5;
+    static final int PROGRESSION_LENGTH_MAX = 10;
     static final int OPERAND_RANGE = 11;
     static final String QUESTION = "What number is missing in the progression?";
 
@@ -15,29 +16,33 @@ public class Progression {
         for (int round = 0; round < Engine.NUM_OF_ROUNDS; round++) {
             int first = Utils.getRandomNum(OPERAND_RANGE * 2); // first num in list 0-22
             int step = Utils.getRandomNum(1, OPERAND_RANGE * 2); // step 1-22
-            int hidden = Utils.getRandomNum(SEQUENCE_LENGTH); // hidden elem 0-9
-            gameData[round][0] = getSequenceQA(first, step, hidden)[0];
-            gameData[round][1] = getSequenceQA(first, step, hidden)[1];
+            int progresLength = Utils.getRandomNum(PROGRESSION_LENGTH_MIN, PROGRESSION_LENGTH_MAX);
+            int hidden = Utils.getRandomNum(progresLength); // hidden elem
+            int[] progression = generateProgression(progresLength, first, step);
+            String[] qa = getQA(progression, hidden);
+            gameData[round][0] = qa[0]; //question
+            gameData[round][1] = qa[1]; //answer
         }
         Engine.run(QUESTION, gameData);
     }
 
-    private static String[] getSequenceQA(int first, int step, int hidden) {
-
-        int[] arrSequence = new int[SEQUENCE_LENGTH];
-        for (int i = 0; i < SEQUENCE_LENGTH; i++) {
-            arrSequence[i] = i * step + first;
+    private static int[] generateProgression (int seqLength, int first, int step){
+        int[] progression = new int[seqLength];
+        for (int i = 0; i < seqLength; i++) {
+            progression[i] = i * step + first;
         }
+        return progression;
+    }
 
+    private static String[] getQA (int[] progression, int hidden) {
         var sequence = new StringJoiner(" ");
-        for (int i = 0; i < SEQUENCE_LENGTH; i++) {
-            sequence.add(i == hidden ? ".." : Integer.toString(arrSequence[i]));
+        for (int i = 0; i < progression.length; i++) {
+            sequence.add(i == hidden ? ".." : Integer.toString(progression[i]));
         }
 
-        String[] sequenceQA = new String[2];
-        sequenceQA[0] = "Question: " + sequence;
-        sequenceQA[1] = Integer.toString(arrSequence[hidden]);
-
-        return sequenceQA;
+        String[] qa = new String[2];
+        qa[0] = "Question: " + sequence;
+        qa[1] = Integer.toString(progression[hidden]);
+        return qa;
     }
 }
